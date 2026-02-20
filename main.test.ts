ddb4d447143f42a3fd70fbd6740770d11e9b1c1b
FedoseevPavelIP415 <mr.jook01@mail.ru>
2026-02-21 02:01:58 +0700

    Тест функции createBook

--- main.test.ts
+++ main.test.ts
@@ -1,6 +1,7 @@
 import { test, expect } from 'vitest';
 import {
   createUser,
+  createBook,
 } from './main';
 
 test('createUser создает пользователя', () => {
@@ -10,4 +11,17 @@ test('createUser создает пользователя', () => {
   expect(user.name).toBe('Pavel');
   expect(user.email).toBe('SenZen@mail.com');
   expect(user.isActive).toBe(true);
+});
+
+test('createBook возвращает книгу', () => {
+  const book = createBook({
+    title: 'Vavilon',
+    author: 'Ktoto',
+    genre: 'Kakoito'
+  });
+
+  expect(book.title).toBe('Vavilon');
+  expect(book.author).toBe('Ktoto');
+  expect(book.genre).toBe('Kakoito');
+  expect(book.year).toBeUndefined();
 });
