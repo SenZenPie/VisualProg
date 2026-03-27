ea546acb468ff6a3bf336949285e7f690c465ce4
FedoseevPavelIP415 <mr.jook01@mail.ru>
2026-03-27 20:12:44 +0700

    Реализован тип Create

--- query.ts
+++ query.ts
@@ -1,1 +1,11 @@
 export type Transform<T> = (data: T[]) => T[];
+
+export type Where<T> = <K extends keyof T>(key: K, value: T[K]) => Transform<T>;
+
+export function createWhere<T>(): Where<T> {
+    return <K extends keyof T>(key: K, value: T[K]): Transform<T> => {
+        return (data: T[]): T[] => {
+            return data.filter(item => item[key] === value);
+        };
+    };
+}
