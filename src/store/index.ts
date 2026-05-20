4e6464d02b4715f18d1d823014a812e08989fb2f
FedoseevPavelIP415 <mr.jook01@mail.ru>
2026-05-20 12:17:06 +0700

    импорты слайсов

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import spreadsheetReducer from './slices/spreadsheetSlice';
+import documentsReducer from './slices/documentsSlice';
+import uiReducer from './slices/uiSlice';
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        spreadsheet: spreadsheetReducer,
+        documents: documentsReducer,
+        ui: uiReducer,
+    },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
