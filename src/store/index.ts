1df05cc1b6dc645563dc03b36b49f639a95b11a3
FedoseevPavelIP415 <mr.jook01@mail.ru>
2026-05-21 18:17:09 +0700

    authSlice

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import spreadsheetReducer from './slices/spreadsheetSlice';
 import documentsReducer from './slices/documentsSlice';
 import uiReducer from './slices/uiSlice';
+import authReducer from './slices/authSlice';
 
 export const store = configureStore({
     reducer: {
         spreadsheet: spreadsheetReducer,
         documents: documentsReducer,
         ui: uiReducer,
+        auth: authReducer,
     },
 });
 
